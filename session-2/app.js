c81594c8aada763799d84ea0516f0424ba34801e
azhar hussain <azhar.hussain@venturedive.com>
2020-02-25 17:48:20 +0500

    assignment 2 complete

--- session-2/app.js
+++ session-2/app.js
@@ -5,27 +5,37 @@ var async = require('express-async-await');
 var fetch = require('node-fetch');
 
 const url = 'https://swapi.co/api/films/';
-router.get('/movies', async function (req, res, next) {
-    function foundData() {
-        return fetch(url);
+const queryVar = 'The Phantom Menace';
+router.get('/movies', (req, res, next) => {
+    try {
+        function foundData() {
+            return fetch(url);
+        }
+        const processData = async () => {
+            const movies = await foundData();
+            const resData = await movies.json();
+            res.send(resData);
+        }
+        processData();
+    } catch (error) {
+        console.log(error);
     }
-    const processData = async () => {
-        const movies = await foundData();
-        const resData = await movies.json();
-        console.log(resData);
-    }
-    processData();
     res.end;
 });
-// fetch(url, { method: 'GET' })
-//     .then((res) => {
-//         return res.json()
-//     })
-//     .then((json) => {
-//         console.log(json);
-//     });
-
-// app.get('', (req, res) => {
-// res.send()
-// })
+router.get('/find_movie', (req, res) => {
+    fetch(url)
+        .then((res) => { return res.json() })
+        .then((data) => {
+            const { results } = data;
+            let filteredData = results.filter(fd => fd.title === queryVar);
+            if (!filteredData.length) {
+                throw new Error('Movie not found')
+            }
+            res.send(filteredData);
+        }).catch((reason) => {
+            res.send(`${reason}`);
+        });
+    res.end;
+});
+app.use(router);
 app.listen(4000, () => console.log('server listening on port 4000'));
